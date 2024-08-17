725aa83d04b606415d916d4ef0938905e6232049
Irine Mariyam Baby <145422368+irinemb@users.noreply.github.com>
2024-08-17 23:00:36 +0530

    Create sqrt.java

---
+++ OOPS/Lab/sqrt.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+class square
+{
+    public static void main(String a[]) 
+    {
+      int a, b;
+      double root, power;
+      Scanner s =new Scanner(System.in);
+      System.out.println("Enter 2 no:s");
+       a=s.nextInt ();
+       b=s.nextint();
+      root=Math.sqrt(a);
+      power =math.pow(a,b) ;
+        System.out.println("The Square root is "+root);
+        System.out.println("The power is "+power)
+     }
+}
