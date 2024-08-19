23bc8272b711139b2562b85d80a28d40710b7814
Irine Mariyam Baby <145422368+irinemb@users.noreply.github.com>
2024-08-19 09:52:44 +0530

    Add files via upload

---
+++ OOPS/Lab/Cycle 2/reverse.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class reverse
+{
+	public static void main(String ar[])
+	{
+    String word,rev="";
+		System.out.println("Enter the string: ");
+    Scanner sc=new Scanner(System.in);
+		word=sc.nextLine();
+		int len=word.length();
+		for(int i=len-1;i>=0;i--)
+		{
+			rev=rev+word.charAt(i);
+		}
+		System.out.println("The reversed string is "+rev);
+	}
+}
